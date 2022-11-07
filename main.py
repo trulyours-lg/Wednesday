c653bc3c1f60b3de3ef53d7bb009316b8b759035
lilygoykhman <lgoykhman@prismhr.com>
2022-11-07 13:07:33 -0500

    Wednesday part 2

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
 import math
 
 #def quadratic_equation_solver(a, b, c): - i didn't have the time to finish both, sorry - i was hoping the user input one will cover both
-def find_largest_and_smallest_numbers(num1=0.0, num2=0.0, num3=0.0): #not finished yet
+def find_largest_and_smallest_numbers(num1=0.0, num2=0.0, num3=0.0):
     str_input="Input the 3 numbers for sorting:"
     q_list = input(str_input).split()
-    if (q_list[0]>q_list[1]):
-        if (q_list[0]>q_list[2]):
-            print(q_list[0],"- the biggest")
+
+    if q_list[0]>q_list[1]:
+        if q_list[0] <= q_list[2]:
+            my_sort =(q_list[2],q_list[1],q_list[0])
+            print(my_sort)
         else:
-            print ("xxx")
+            if q_list[2] > q_list[1]:
+               my_sort =(q_list[1],q_list[2],q_list[0])
+               print(my_sort)
+    elif q_list[0]<q_list[1]:
+        if q_list[0]<q_list[2]:
+            if q_list[1]<q_list[2]:
+                my_sort =(q_list[0],q_list[1],q_list[2])
+                print(my_sort)
+            else:
+                my_sort=(q_list[0],q_list[2],q_list[1])
+                print(my_sort)
+        else:
+            my_sort=(q_list[2],q_list[0],q_list[1])
+            print(my_sort)
+
 #def temp_checker(min_temp, temp_1, temp_2, temp_3):
 
 def calc_math_expression(num1, num2, operator):
@@ -49,8 +65,8 @@ def quadratic_equation_solver_from_user_input():
 
 str_input= "Input your single digit numbers and math operation:"
 my_input = input(str_input)
-calc_math_expression_from_str(my_input)
+#calc_math_expression_from_str(my_input)
 #---------------------------------------------------
 # do the  quadratic expression solver
-quadratic_equation_solver_from_user_input()
+#quadratic_equation_solver_from_user_input()
 find_largest_and_smallest_numbers(num1=0.0, num2=0.0, num3=0.0)
